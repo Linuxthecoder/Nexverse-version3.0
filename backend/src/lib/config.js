538989f246b91f1579a5e9bbaf986c174e9c5457
ByteSpecter <198821104+Linuxthecoder@users.noreply.github.com>
2025-08-07 00:36:26 +0530

    Delete backend directory

--- backend/src/lib/config.js
+++
@@ -1,9 +0,0 @@
-export const MONGODB_URI = 'mongodb+srv://Nexora:7Ib1bRpd3RtXe0nV@galaxycluster01.8pz68zq.mongodb.net/NexCommunityHostory?retryWrites=true&w=majority&appName=Galaxycluster01';
-
-export const JWT_SECRET = '857cc76780c51bce22579fe1fcafa2796a43427242cbbc8837f97e3437ea83d5';
-
-export const CLOUDINARY_CONFIG = {
-    cloud_name: 'dfpb7rgll',
-    api_key: '674644266213533',
-    api_secret: '_0HK2vQmiAcUnpvrySSqBT6OgNs'
-}; 
